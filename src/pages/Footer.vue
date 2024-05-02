de9e3a8211935858296ad2468557b9b784096952
doris0728 <doris07280728@gmail.com>
2024-05-02 10:31:36 +0800

    footer ing

--- src/pages/Footer.vue
+++ src/pages/Footer.vue
@@ -4,15 +4,25 @@
 
 <template>
     <div class="container">
-        <font-awesome-icon :icon="['fas', 'house']" />
-        <font-awesome-icon :icon="['fas', 'house']" />
-123
+        <font-awesome-icon class="footerIcon" :icon="['fas', 'house']" />
+        <font-awesome-icon class="footerIcon" :icon="['fas', 'magnifying-glass']" />
+        <font-awesome-icon class="footerIcon" :icon="['fas', 'plus']" />
+        <font-awesome-icon class="footerIcon" :icon="['fas', 'envelope']" />
+        <!-- <font-awesome-icon :icon="['fas', 'bell']" /> -->
+        <font-awesome-icon class="footerIcon" :icon="['fas', 'user-large']" />
     </div>
 </template>
 
 <style lang="scss" scoped>
-    .container {
-        background-color: gray;
-        /* height: 70px; */
-    }
+.container {
+    background-color: gray;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
+<style>
+.footerIcon.svg-inline--fa {
+    height: 1.8em;
+}
 </style>
