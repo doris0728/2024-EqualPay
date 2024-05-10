c8b8d7633d878ed94addf26a3fcc4d189c1207fd
JessieHsu <>
2024-05-10 17:42:52 +0800

    Add ProjectOverview and AccountPage

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ const About = { template: '<div>About</div>' }
 const routes = [
   { path: '/', component: () => import('./pages/Home.vue') },
   { path: '/ProjectList', component: () => import('./pages/ProjectList.vue') },
+  { path: '/ProjectOverview', component: () => import('./pages/ProjectOverview.vue') },
+  { path: '/AccountPage', component: () => import('./pages/AccountPage.vue') },
   
 //   { path: '/about', component: About },
 ]
